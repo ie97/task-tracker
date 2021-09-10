8f253dec1bf03fc2b77c29efd741a1b19f39ccfd
ie97 <iwan.ie.id@gmail.com>
2021-09-10 09:43:22 +0700

    without json server

--- src/App.js
+++ src/App.js
@@ -1,37 +1,66 @@
-import Header from "./components/Header";
-import Tasks from "./components/Tasks";
+import Header from "./components/Header"
+import Tasks from "./components/Tasks"
 import { useState } from "react"
+import AddTask from "./components/AddTask"
 
 const App = () => {
-  
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
       id: 1,
-      text: 'Doctor Appointment',
-      day: 'Feb 5th at 2:30pm',
-      reminder:true,
+      text: "Doctor Appointment",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
     },
     {
       id: 2,
-      text: 'Meeting at School',
-      day: 'Feb 6th at 1:30pm',
-      reminder:true,
+      text: "Meeting at School",
+      day: "Feb 6th at 1:30pm",
+      reminder: true,
     },
     {
       id: 3,
-      text: 'Food Shopping',
-      day: 'Feb 5th at 2:30pm',
-      reminder:false,
+      text: "Food Shopping",
+      day: "Feb 5th at 2:30pm",
+      reminder: false,
     },
   ])
-  
+
+  // Add task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    )
+  }
+
   return (
     <div className="container">
-      <Header title='Task Tracker' />
-      <Tasks tasks={tasks}/>
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No Task To Show"
+      )}
     </div>
-  );
+  )
 }
- 
-export default App;
 
+export default App
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,20 @@
 import Button from "./Button"
 
-const Header = ( {title} ) => {
-  const onClick = (e) => {
-    console.log(e)
-  }
-  
+const Header = ({ title, onAdd, showAdd }) => {
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button color='green' text='Add' onClick={onClick}/>
-
+      <Button
+        color={showAdd ? "red" : "green"}
+        text={showAdd ? "Close" : "Add"}
+        onClick={onAdd}
+      />
     </header>
   )
 }
 
 Header.defaultProps = {
-  title: 'Task Tracker',
+  title: "Task Tracker",
 }
 
 // css in JS
@@ -23,7 +22,6 @@ Header.defaultProps = {
 //   color: 'red',
 //   backgroundColor: 'black',
 // }
-//<h1 style={headingStyle}>{title}</h1> 
+//<h1 style={headingStyle}>{title}</h1>
 
 export default Header
-
